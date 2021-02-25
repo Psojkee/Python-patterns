93538efc55061ee9ce195935b63c0707d293c9c4
Psojkee <37179553+Psojkee@users.noreply.github.com>
2021-02-25 18:45:24 +0700

    Delete JSONparse.py

--- JSONparse.py
+++
@@ -1,9 +0,0 @@
-import json
-import urllib.request, urllib.parse, urllib.error
-serviceurl='http://py4e-data.dr-chuck.net/comments_792975.json'
-data=urllib.request.urlopen(serviceurl).read()
-info=json.loads(data)
-cnt=0
-for line in info['comments']:
-    cnt+=int(line['count'])
-print(cnt)
